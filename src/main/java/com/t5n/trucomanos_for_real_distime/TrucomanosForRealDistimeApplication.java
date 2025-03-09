dfbd6ad972e6f9f9b1ffe31080dbca85090c3c91
Bruno <95176651+koddoki@users.noreply.github.com>
2025-03-09 09:22:17 -0300

    primeirus commitus

---
+++ src/main/java/com/t5n/trucomanos_for_real_distime/TrucomanosForRealDistimeApplication.java
@@ -0,0 +1,13 @@
+package com.t5n.trucomanos_for_real_distime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrucomanosForRealDistimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrucomanosForRealDistimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t5n/trucomanos_for_real_distime/TrucomanosForRealDistimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t5n.trucomanos_for_real_distime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrucomanosForRealDistimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
